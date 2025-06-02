cc788504353eb70835aded16e53830e052f2dd34
Sergi Arguimbau <sergiarguimbau@gmail.com>
2025-06-02 19:16:58 +0200

    Refactors project structure and database initialization
    Moves environment files and initialization scripts to improve project organization.
    
    Simplifies database initialization and corrects a type definition in the vessel table.
    
    Adds a convenience script command to the `package.json` to run the project.

--- backend/docker-init/pgdb-init.sh
+++ backend/docker-init/pgdb-init.sh
@@ -9,7 +9,7 @@ EOSQL
 # Create the vessels table if it does not exist already
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE TABLE IF NOT EXISTS vessels (
-        mmsi        BIGINT PRIMARY KEY,               -- Unique vessel ID (MMSI)
+        mmsi        INTEGER PRIMARY KEY,              -- Vessel User ID (MMSI)
         position    geometry(Point, 4326) NOT NULL,   -- Geo point (lon/lat, WGS84)
         heading     INTEGER,                          -- Direction in degrees
         updated_at  TIMESTAMPTZ DEFAULT NOW()         -- Last update timestamp
@@ -17,7 +17,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 
     -- Spatial index for fast bounding box queries
     CREATE INDEX IF NOT EXISTS idx_vessels_position ON vessels USING GIST (position);
-    -- Recency index for quick filtering
+    -- Recency index for quick time filtering
     CREATE INDEX IF NOT EXISTS idx_vessels_updated_at ON vessels (updated_at);
 EOSQL
-
