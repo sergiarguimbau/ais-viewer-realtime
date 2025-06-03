ad3a622e49f99cb59780733e5d76218ef4dcc2b5
Sergi Arguimbau <sergiarguimbau@gmail.com>
2025-06-04 00:54:07 +0200

    Display Mapbox map securely with dotenv API key

--- frontend/mobile/AISViewer/App.tsx
+++ frontend/mobile/AISViewer/App.tsx
@@ -1,6 +1,34 @@
+import Mapbox from "@rnmapbox/maps";
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Config from "react-native-config";
+
+Mapbox.setAccessToken(Config.MAPBOX_API_KEY);
 
 export default function App() {
-  return <View />;
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.mapContainer}>
+        <Mapbox.MapView
+          style={styles.mapView}
+          styleURL={Mapbox.StyleURL.Light}
+          rotateEnabled={false}
+          compassEnabled
+        />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+  mapContainer: {
+    height: "100%",
+    width: "100%",
+  },
+  mapView: {
+    flex: 1,
+  },
+});
